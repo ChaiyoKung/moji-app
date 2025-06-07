abdb1da51988d17c519eb5429fba8b155efc7142
ChaiyoKung <kananek45@gmail.com>
2025-06-08 03:23:12 +0700

    feat: integrate gluestack-ui

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import "./global.css";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { GluestackUIProvider } from "./components/ui/gluestack-ui-provider";
+import { Text } from "./components/ui/text";
+import { Box } from "./components/ui/box";
 
 export default function App() {
   return (
-    <View className="flex-1 bg-white items-center justify-center">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider>
+      <Box className="flex-1 bg-white items-center justify-center">
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </Box>
+    </GluestackUIProvider>
   );
 }
