8902e0581f067f9d643e142af41134f6e584e36c
ChaiyoKung <kananek45@gmail.com>
2025-06-08 20:45:25 +0700

    feat: login screen ui

--- App.tsx
+++ App.tsx
@@ -1,16 +1,35 @@
 import "./global.css";
 import { StatusBar } from "expo-status-bar";
 import { GluestackUIProvider } from "./components/ui/gluestack-ui-provider";
-import { Text } from "./components/ui/text";
-import { Box } from "./components/ui/box";
+import { Button, ButtonText } from "./components/ui/button";
+import { Heading } from "./components/ui/heading";
+import { VStack } from "./components/ui/vstack";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Center } from "./components/ui/center";
+import { Image } from "./components/ui/image";
 
 export default function App() {
   return (
     <GluestackUIProvider>
-      <Box className="flex-1 bg-white items-center justify-center">
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </Box>
+      <SafeAreaView edges={["top"]} className="flex-1 bg-gray-100">
+        <Center className="px-6 py-32">
+          <Image
+            source={require("./assets/moji-logo.png")}
+            alt="Moji Logo"
+            resizeMode="contain"
+            className="w-48 h-48"
+          />
+        </Center>
+        <VStack space="lg" className="flex-1 bg-white p-6 rounded-t-3xl">
+          <Heading size="3xl" className="my-4">
+            เข้าสู่ระบบ
+          </Heading>
+          <Button className="rounded-2xl">
+            <ButtonText>เข้าสู่ระบบด้วย Google</ButtonText>
+          </Button>
+          <StatusBar style="auto" />
+        </VStack>
+      </SafeAreaView>
     </GluestackUIProvider>
   );
 }
