5e7d1721940916dc62f2cfde2fb03bfefd5848c7
ChaiyoKung <kananek45@gmail.com>
2025-06-09 00:43:37 +0700

    feat: home total expense card ui

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -2,6 +2,8 @@ import { Text } from "../../components/ui/text";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Heading } from "../../components/ui/heading";
 import { VStack } from "../../components/ui/vstack";
+import { Box } from "../../components/ui/box";
+import { Center } from "../../components/ui/center";
 
 export default function Home() {
   return (
@@ -13,6 +15,19 @@ export default function Home() {
             19,934
           </Heading>
         </VStack>
+
+        <Box className="bg-white rounded-2xl border border-gray-200 overflow-hidden">
+          <Center className="px-4 py-2 border-b border-gray-200 bg-red-100">
+            <Text bold className="text-red-800">
+              รายจ่าย วันนี้
+            </Text>
+          </Center>
+          <Center className="p-4">
+            <Text size="4xl" bold>
+              66
+            </Text>
+          </Center>
+        </Box>
       </VStack>
     </SafeAreaView>
   );
