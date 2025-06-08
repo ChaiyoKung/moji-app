2f10195dfd4b3d64485432fcf50a6ecca9745f90
ChaiyoKung <kananek45@gmail.com>
2025-06-09 00:42:47 +0700

    feat: home balance ui

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,10 +1,19 @@
 import { Text } from "../../components/ui/text";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Heading } from "../../components/ui/heading";
+import { VStack } from "../../components/ui/vstack";
 
 export default function Home() {
   return (
     <SafeAreaView edges={["top"]} className="flex-1 bg-gray-100">
-      <Text>Home</Text>
+      <VStack space="md" className="p-4">
+        <VStack>
+          <Text className="text-gray-500">เงินคงเหลือ</Text>
+          <Heading size="xl" className="text-green-500">
+            19,934
+          </Heading>
+        </VStack>
+      </VStack>
     </SafeAreaView>
   );
 }
