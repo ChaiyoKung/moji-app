750aa85442ba9b0597494cb82baa17e769ddf508
ChaiyoKung <kananek45@gmail.com>
2025-06-09 23:28:43 +0700

    feat: home scrollable

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -4,27 +4,30 @@ import { VStack } from "../../components/ui/vstack";
 import { BalanceSummary } from "../../components/balance-summary";
 import { SummaryCard } from "../../components/summary-card";
 import { Calendar, LocaleConfig } from "react-native-calendars";
+import { ScrollView } from "react-native";
 
 LocaleConfig.defaultLocale = "th";
 
 export default function Home() {
   return (
     <SafeAreaView edges={["top"]} className="flex-1 bg-gray-100">
-      <VStack space="md" className="p-4">
-        <BalanceSummary label="เงินคงเหลือ" value={19934} />
+      <ScrollView>
+        <VStack space="md" className="p-4">
+          <BalanceSummary label="เงินคงเหลือ" value={19934} />
 
-        <SummaryCard label="รายจ่าย วันนี้" value={66} />
+          <SummaryCard label="รายจ่าย วันนี้" value={66} />
 
-        <Calendar
-          theme={{
-            calendarBackground: "transparent",
-            todayBackgroundColor: "#0da6f2",
-            todayTextColor: "#ffffff",
-            todayDotColor: "#ffffff",
-            arrowColor: "#9e9e9e",
-          }}
-        />
-      </VStack>
+          <Calendar
+            theme={{
+              calendarBackground: "transparent",
+              todayBackgroundColor: "#0da6f2",
+              todayTextColor: "#ffffff",
+              todayDotColor: "#ffffff",
+              arrowColor: "#9e9e9e",
+            }}
+          />
+        </VStack>
+      </ScrollView>
     </SafeAreaView>
   );
 }
