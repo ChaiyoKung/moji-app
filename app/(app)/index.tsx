c31f43f6bee2ce3c087af2892d53fcb5f384190c
ChaiyoKung <kananek45@gmail.com>
2025-06-09 22:21:46 +0700

    refactor: extract into `SummaryCard` component

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,10 +1,8 @@
 import "../../libs/calendar-locale-config.th";
-import { Text } from "../../components/ui/text";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { VStack } from "../../components/ui/vstack";
 import { BalanceSummary } from "../../components/balance-summary";
-import { Box } from "../../components/ui/box";
-import { Center } from "../../components/ui/center";
+import { SummaryCard } from "../../components/summary-card";
 import { Calendar, LocaleConfig } from "react-native-calendars";
 
 LocaleConfig.defaultLocale = "th";
@@ -15,18 +13,7 @@ export default function Home() {
       <VStack space="md" className="p-4">
         <BalanceSummary label="เงินคงเหลือ" value={19934} />
 
-        <Box className="bg-white rounded-2xl border border-gray-200 overflow-hidden">
-          <Center className="px-4 py-2 border-b border-gray-200 bg-red-100">
-            <Text bold className="text-red-800">
-              รายจ่าย วันนี้
-            </Text>
-          </Center>
-          <Center className="p-4">
-            <Text size="4xl" bold>
-              66
-            </Text>
-          </Center>
-        </Box>
+        <SummaryCard label="รายจ่าย วันนี้" value={66} />
 
         <Calendar
           theme={{
---
+++ components/summary-card/index.tsx
@@ -0,0 +1,26 @@
+import { formatBaht } from "../../utils/format-baht";
+import { Box } from "../ui/box";
+import { Center } from "../ui/center";
+import { Text } from "../ui/text";
+
+export interface SummaryCardProps {
+  label: string;
+  value: number;
+}
+
+export function SummaryCard({ label, value }: SummaryCardProps) {
+  return (
+    <Box className="bg-white rounded-2xl border border-gray-200 overflow-hidden">
+      <Center className="px-4 py-2 border-b border-gray-200 bg-red-100">
+        <Text bold className="text-red-800">
+          {label}
+        </Text>
+      </Center>
+      <Center className="p-4">
+        <Text size="4xl" bold>
+          {formatBaht(value)}
+        </Text>
+      </Center>
+    </Box>
+  );
+}
