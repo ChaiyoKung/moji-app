188df01f1029d27ca972eed74b16b74ec526ac67
ChaiyoKung <kananek45@gmail.com>
2025-06-11 23:27:14 +0700

    feat: add `onPress` prop to `AddIncomeFab` and `AddExpenseFab` components

--- components/add-expense-fab/index.tsx
+++ components/add-expense-fab/index.tsx
@@ -1,12 +1,16 @@
+import { PressableProps } from "react-native";
 import { Fab, FabIcon, FabLabel } from "../ui/fab";
 import { AddIcon } from "../ui/icon";
 
-export function AddExpenseFab() {
+export type AddExpenseFabProps = Pick<PressableProps, "onPress">;
+
+export function AddExpenseFab({ onPress }: AddExpenseFabProps) {
   return (
     <Fab
       size="lg"
       placement="bottom right"
       className="rounded-2xl bg-red-500 hover:bg-red-600 active:bg-red-700"
+      onPress={onPress}
     >
       <FabLabel bold>เพิ่มรายจ่าย</FabLabel>
       <FabIcon as={AddIcon} />
--- components/add-income-fab/index.tsx
+++ components/add-income-fab/index.tsx
@@ -1,12 +1,16 @@
+import { PressableProps } from "react-native";
 import { Fab, FabIcon, FabLabel } from "../ui/fab";
 import { AddIcon } from "../ui/icon";
 
-export function AddIncomeFab() {
+export type AddIncomeFabProps = Pick<PressableProps, "onPress">;
+
+export function AddIncomeFab({ onPress }: AddIncomeFabProps) {
   return (
     <Fab
       size="lg"
       placement="bottom left"
       className="rounded-2xl bg-green-500 hover:bg-green-600 active:bg-green-700"
+      onPress={onPress}
     >
       <FabIcon as={AddIcon} />
       <FabLabel bold>เพิ่มรายรับ</FabLabel>
