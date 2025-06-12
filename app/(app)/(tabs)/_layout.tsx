b446420a171580735cfdd581d4294a126d59274e
ChaiyoKung <kananek45@gmail.com>
2025-06-12 21:32:18 +0700

    feat: styling bottom tabs

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -1,8 +1,102 @@
 import { Tabs } from "expo-router";
+import { HStack } from "../../../components/ui/hstack";
+import { Text } from "../../../components/ui/text";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Icon } from "../../../components/ui/icon";
+import {
+  ChartNoAxesColumnIcon,
+  CircleUserRoundIcon,
+  NotebookPenIcon,
+  ShieldAlertIcon,
+} from "lucide-react-native";
+import { VStack } from "../../../components/ui/vstack";
+import { Pressable } from "react-native";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+      screenOptions={{ headerShown: false }}
+      tabBar={({ state, descriptors, navigation }) => {
+        return (
+          <SafeAreaView
+            edges={["bottom"]}
+            className="bg-white border-t border-gray-200 rounded-t-2xl overflow-hidden"
+          >
+            <HStack>
+              {state.routes.map((route, index) => {
+                const { options } = descriptors[route.key];
+
+                const label =
+                  options.title !== undefined ? options.title : route.name;
+
+                const isFocused = state.index === index;
+
+                const onPress = () => {
+                  const event = navigation.emit({
+                    type: "tabPress",
+                    target: route.key,
+                    canPreventDefault: true,
+                  });
+
+                  if (!isFocused && !event.defaultPrevented) {
+                    navigation.navigate(route.name, route.params);
+                  }
+                };
+
+                const onLongPress = () => {
+                  navigation.emit({
+                    type: "tabLongPress",
+                    target: route.key,
+                  });
+                };
+
+                const iconComponent =
+                  route.name === "index"
+                    ? NotebookPenIcon
+                    : route.name === "dashboard"
+                      ? ChartNoAxesColumnIcon
+                      : route.name === "profile"
+                        ? CircleUserRoundIcon
+                        : ShieldAlertIcon;
+
+                return (
+                  <Pressable
+                    key={route.key}
+                    onPress={onPress}
+                    onLongPress={onLongPress}
+                    accessibilityState={isFocused ? { selected: true } : {}}
+                    accessibilityLabel={options.tabBarAccessibilityLabel}
+                    testID={options.tabBarButtonTestID}
+                    className="flex-1 pt-3 pb-2"
+                  >
+                    {({ pressed }) => {
+                      const textColor = isFocused
+                        ? "text-blue-500"
+                        : pressed
+                          ? "text-gray-700"
+                          : "text-gray-500";
+
+                      return (
+                        <VStack space="xs" className="items-center">
+                          <Icon
+                            as={iconComponent}
+                            size="xl"
+                            className={textColor}
+                          />
+                          <Text size="sm" className={textColor}>
+                            {label}
+                          </Text>
+                        </VStack>
+                      );
+                    }}
+                  </Pressable>
+                );
+              })}
+            </HStack>
+          </SafeAreaView>
+        );
+      }}
+    >
       <Tabs.Screen name="index" options={{ title: "บันทึก" }} />
       <Tabs.Screen name="dashboard" options={{ title: "สรุป" }} />
       <Tabs.Screen name="profile" options={{ title: "โปรไฟล์" }} />
