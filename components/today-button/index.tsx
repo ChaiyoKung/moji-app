f128008c414a2bb2dbaa1e18b97bc10adaba492b
ChaiyoKung <kananek45@gmail.com>
2025-06-13 23:22:07 +0700

    feat: add onPress prop to TodayButton component

--- components/today-button/index.tsx
+++ components/today-button/index.tsx
@@ -1,11 +1,17 @@
 import { CalendarDaysIcon } from "lucide-react-native";
 import { Button, ButtonIcon, ButtonText } from "../ui/button";
+import { InterfaceButtonProps } from "@gluestack-ui/button/lib/types";
 
-export function TodayButton() {
+export interface TodayButtonProps {
+  onPress?: InterfaceButtonProps["onPress"];
+}
+
+export function TodayButton({ onPress }: TodayButtonProps) {
   return (
     <Button
       variant="outline"
       className="rounded-2xl border-teal-500 data-[hover=true]:border-teal-600 data-[active=true]:border-teal-700"
+      onPress={onPress}
     >
       <ButtonIcon
         as={CalendarDaysIcon}
