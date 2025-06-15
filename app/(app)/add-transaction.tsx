4a8e8e001b6367b93c6577855f655bffab25ccf2
ChaiyoKung <kananek45@gmail.com>
2025-06-15 17:05:40 +0700

    feat: add save button and handle transaction saving in Transaction component

--- app/(app)/add-transaction.tsx
+++ app/(app)/add-transaction.tsx
@@ -9,6 +9,9 @@ import { Input, InputField } from "../../components/ui/input";
 import { useState } from "react";
 import { formatBaht } from "../../utils/format-baht";
 import colors from "tailwindcss/colors";
+import { Button, ButtonIcon, ButtonText } from "../../components/ui/button";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { SaveIcon } from "lucide-react-native";
 
 const categories = [
   {
@@ -149,83 +152,107 @@ export default function Transaction() {
   const [amount, setAmount] = useState<string>("");
   const [note, setNote] = useState<string>("");
 
+  const handleSave = () => {
+    console.log("Transaction saved:", {
+      mode,
+      date,
+      category: selectedCatagoryId,
+      amount: parseFloat(amount),
+      note,
+    });
+  };
   return (
-    <ScrollView className="flex-1 bg-gray-100">
-      <VStack space="md" className="p-4">
-        <VStack>
-          <Heading size="3xl" className="text-typography-black">
-            {mode === "income" ? "เพิ่มรายรับ" : "เพิ่มรายจ่าย"}
-          </Heading>
-          <Text>{`ของวันนี้ (${date})`}</Text>
-        </VStack>
+    <>
+      <ScrollView className="flex-1 bg-gray-100">
+        <VStack space="md" className="p-4">
+          <VStack>
+            <Heading size="3xl" className="text-typography-black">
+              {mode === "income" ? "เพิ่มรายรับ" : "เพิ่มรายจ่าย"}
+            </Heading>
+            <Text>{`ของวันนี้ (${date})`}</Text>
+          </VStack>
 
-        <VStack space="sm">
-          <Heading bold className="text-typography-black">
-            ประเภท
-          </Heading>
-          <HStack space="sm" className="flex-wrap">
-            {categories.map((category) => (
-              <Pressable
-                key={category._id}
-                className="flex-row items-center border rounded-full px-4 py-1"
-                style={{
-                  borderColor: category.color,
-                  backgroundColor:
-                    selectedCatagoryId === category._id
-                      ? category.color
-                      : "transparent",
-                }}
-                onPress={() => setSelectedCatagoryId(category._id)}
-              >
-                <Text className="text-lg">{category.icon}</Text>
-                <Text
-                  className="ml-2 text-lg"
+          <VStack space="sm">
+            <Heading bold className="text-typography-black">
+              ประเภท
+            </Heading>
+            <HStack space="sm" className="flex-wrap">
+              {categories.map((category) => (
+                <Pressable
+                  key={category._id}
+                  className="flex-row items-center border rounded-full px-4 py-1"
                   style={{
-                    color:
+                    borderColor: category.color,
+                    backgroundColor:
                       selectedCatagoryId === category._id
-                        ? colors.white
-                        : category.color,
+                        ? category.color
+                        : "transparent",
                   }}
+                  onPress={() => setSelectedCatagoryId(category._id)}
                 >
-                  {category.name}
-                </Text>
-              </Pressable>
-            ))}
-          </HStack>
-        </VStack>
+                  <Text className="text-lg">{category.icon}</Text>
+                  <Text
+                    className="ml-2 text-lg"
+                    style={{
+                      color:
+                        selectedCatagoryId === category._id
+                          ? colors.white
+                          : category.color,
+                    }}
+                  >
+                    {category.name}
+                  </Text>
+                </Pressable>
+              ))}
+            </HStack>
+          </VStack>
 
-        <VStack space="sm">
-          <Heading bold className="text-typography-black">
-            จำนวนเงิน
-          </Heading>
-          <Input className="rounded-2xl bg-gray-200">
-            <InputField
-              type="text"
-              placeholder="0"
-              value={amount}
-              onChangeText={(text) => setAmount(text)}
-              keyboardType="numeric"
-            />
-          </Input>
-          <Text className="text-teal-500">
-            {`เงินคงเหลือ ${formatBaht(19934)}`}
-          </Text>
-        </VStack>
+          <VStack space="sm">
+            <Heading bold className="text-typography-black">
+              จำนวนเงิน
+            </Heading>
+            <Input className="rounded-2xl bg-gray-200">
+              <InputField
+                type="text"
+                placeholder="0"
+                value={amount}
+                onChangeText={(text) => setAmount(text)}
+                keyboardType="numeric"
+              />
+            </Input>
+            <Text className="text-teal-500">
+              {`เงินคงเหลือ ${formatBaht(19934)}`}
+            </Text>
+          </VStack>
 
-        <VStack space="sm">
-          <Heading bold className="text-typography-black">
-            บันทึกช่วยจำ
-          </Heading>
-          <Input className="rounded-2xl">
-            <InputField
-              type="text"
-              placeholder="เพิ่มข้อความ..."
-              value={note}
-              onChangeText={(text) => setNote(text)}
-            />
-          </Input>
+          <VStack space="sm">
+            <Heading bold className="text-typography-black">
+              บันทึกช่วยจำ
+            </Heading>
+            <Input className="rounded-2xl">
+              <InputField
+                type="text"
+                placeholder="เพิ่มข้อความ..."
+                value={note}
+                onChangeText={(text) => setNote(text)}
+              />
+            </Input>
+          </VStack>
         </VStack>
-      </VStack>
-    </ScrollView>
+      </ScrollView>
+
+      <SafeAreaView
+        edges={["bottom"]}
+        className="p-4 bg-white border-t border-gray-200 rounded-t-2xl overflow-hidden"
+      >
+        <Button
+          className="rounded-2xl bg-blue-500 data-[hover=true]:bg-blue-600 data-[active=true]:bg-blue-700"
+          onPress={handleSave}
+        >
+          <ButtonIcon as={SaveIcon} />
+          <ButtonText>บันทึก</ButtonText>
+        </Button>
+      </SafeAreaView>
+    </>
   );
 }
