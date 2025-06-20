2e0d9ac423eb3fa7ec378e9d9dd4992f889fb443
ChaiyoKung <kananek45@gmail.com>
2025-06-21 03:43:36 +0700

    refactor: add gap to CategoryChip component for improved layout

--- src/components/category-chip/index.tsx
+++ src/components/category-chip/index.tsx
@@ -13,17 +13,16 @@ export interface CategoryChipProps {
 export function CategoryChip({ data, selected, onPress }: CategoryChipProps) {
   return (
     <Pressable
-      className="flex-row items-center border rounded-full px-4 py-1"
+      className="flex-row items-center gap-2 border rounded-full px-4 py-1"
       style={{
         borderColor: data.color,
         backgroundColor: selected ? data.color : "transparent",
       }}
       onPress={onPress}
     >
-      <Text size="lg">{data.icon}</Text>
+      {data.icon !== undefined && <Text size="lg">{data.icon}</Text>}
       <Text
         size="lg"
-        className="ml-2"
         style={{
           color: selected ? colors.white : data.color,
         }}
