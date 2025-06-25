e7c2c82fa992ffb1d41ee86bf3af2c55b625b335
ChaiyoKung <kananek45@gmail.com>
2025-06-26 00:39:07 +0700

    feat: enhance response interceptor to handle token refresh logic

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { env } from "../env";
-import { getStorageItemAsync } from "../hooks/use-storage-state";
+import {
+  getStorageItemAsync,
+  setStorageItemAsync,
+} from "../hooks/use-storage-state";
 import * as AxiosLogger from "axios-logger";
 
 export const api = axios.create({ baseURL: env.EXPO_PUBLIC_API_URL });
@@ -22,10 +25,43 @@ api.interceptors.request.use(
   }
 );
 
-api.interceptors.response.use((response) => {
-  return AxiosLogger.responseLogger(response, {
-    dateFormat: "isoDateTime",
-    data: false,
-    params: true,
-  });
-});
+api.interceptors.response.use(
+  (response) => {
+    return AxiosLogger.responseLogger(response, {
+      dateFormat: "isoDateTime",
+      data: false,
+      params: true,
+    });
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = await getStorageItemAsync("refreshToken");
+        if (refreshToken) {
+          console.warn("Refreshing token...");
+          const refreshTokenResponse = await axios.post<{
+            accessToken: string;
+            refreshToken: string;
+          }>(`${env.EXPO_PUBLIC_API_URL}/auth/refresh`, { refreshToken });
+          const data = refreshTokenResponse.data;
+          const newAccessToken = data.accessToken;
+          const newRefreshToken = data.refreshToken;
+          await setStorageItemAsync("accessToken", newAccessToken);
+          await setStorageItemAsync("refreshToken", newRefreshToken);
+          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+          return api(originalRequest);
+        }
+      } catch (refreshError) {
+        console.error("Error refreshing token:", refreshError);
+        throw refreshError;
+      }
+    }
+    return Promise.reject(error);
+  }
+);
