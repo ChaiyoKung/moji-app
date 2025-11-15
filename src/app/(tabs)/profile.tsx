cd74db26851b38cc3d240ae81c57e870863c7585
ChaiyoKung <kananek45@gmail.com>
2025-11-16 01:52:40 +0700

    feat(profile): improve settings UI and add descriptions for switches
    Refactored profile screen to use ScrollView for better scrolling. Enhanced settings layout with additional descriptions for auto-focus switches, improving clarity for users. This update makes the UI more informative and user-friendly.

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -9,6 +9,7 @@ import { Text } from "../../components/ui/text";
 import { Switch } from "../../components/ui/switch";
 import colors from "tailwindcss/colors";
 import { useSettingStore } from "../../stores/use-setting-store";
+import { ScrollView } from "react-native";
 
 export default function Profile() {
   const isBalanceHidden = useSettingStore((state) => state.isBalanceHidden);
@@ -26,60 +27,84 @@ export default function Profile() {
 
   return (
     <SafeAreaView edges={["top"]} className="flex-1 bg-background-100">
-      <VStack space="4xl" className="flex-1 p-4">
-        <VStack space="md">
-          <ProfileDetails />
-        </VStack>
+      <ScrollView>
+        <VStack space="4xl" className="p-4">
+          <VStack space="md">
+            <ProfileDetails />
+          </VStack>
 
-        <VStack space="md" className="flex-1">
-          <HStack className="items-center rounded-2xl border border-gray-300 px-4 py-2">
-            <Text className="flex-1">ซ่อนเงินคงเหลือ</Text>
-            <Switch
-              value={isBalanceHidden}
-              onToggle={toggleHideBalance}
-              trackColor={{
-                false: colors.blue[300],
-                true: colors.blue[500],
-              }}
-              thumbColor={colors.blue[100]}
-              ios_backgroundColor={colors.blue[300]}
-            />
-          </HStack>
-          <HStack className="items-center rounded-2xl border border-gray-300 px-4 py-2">
-            <Text className="flex-1">โฟกัสจำนวนเงินอัตโนมัติ</Text>
-            <Switch
-              value={isAutoFocusAmount}
-              onToggle={toggleAutoFocusAmount}
-              trackColor={{
-                false: colors.blue[300],
-                true: colors.blue[500],
-              }}
-              thumbColor={colors.blue[100]}
-              ios_backgroundColor={colors.blue[300]}
-            />
-          </HStack>
-          <HStack className="items-center rounded-2xl border border-gray-300 px-4 py-2">
-            <Text className="flex-1">โฟกัสบันทึกช่วยจำอัตโนมัติ</Text>
-            <Switch
-              value={isAutoFocusNote}
-              onToggle={toggleAutoFocusNote}
-              trackColor={{
-                false: colors.blue[300],
-                true: colors.blue[500],
-              }}
-              thumbColor={colors.blue[100]}
-              ios_backgroundColor={colors.blue[300]}
-            />
-          </HStack>
-        </VStack>
+          <VStack space="md" className="flex-1">
+            <HStack
+              space="sm"
+              className="items-center rounded-2xl border border-gray-300 px-4 py-2"
+            >
+              <VStack className="flex-1">
+                <Text>ซ่อนเงินคงเหลือ</Text>
+              </VStack>
+              <Switch
+                value={isBalanceHidden}
+                onToggle={toggleHideBalance}
+                trackColor={{
+                  false: colors.blue[300],
+                  true: colors.blue[500],
+                }}
+                thumbColor={colors.blue[100]}
+                ios_backgroundColor={colors.blue[300]}
+              />
+            </HStack>
+            <HStack
+              space="sm"
+              className="items-center rounded-2xl border border-gray-300 px-4 py-2"
+            >
+              <VStack className="flex-1">
+                <Text>โฟกัสจำนวนเงินอัตโนมัติ</Text>
+                <Text size="xs" className="text-gray-500">
+                  เมื่อเลือกประเภทแล้ว ระบบจะโฟกัสช่อง “จำนวนเงิน” อัตโนมัติ
+                </Text>
+              </VStack>
+              <Switch
+                value={isAutoFocusAmount}
+                onToggle={toggleAutoFocusAmount}
+                trackColor={{
+                  false: colors.blue[300],
+                  true: colors.blue[500],
+                }}
+                thumbColor={colors.blue[100]}
+                ios_backgroundColor={colors.blue[300]}
+              />
+            </HStack>
+            <HStack
+              space="sm"
+              className="items-center rounded-2xl border border-gray-300 px-4 py-2"
+            >
+              <VStack className="flex-1">
+                <Text>โฟกัสบันทึกช่วยจำอัตโนมัติ</Text>
+                <Text size="xs" className="text-gray-500">
+                  เมื่อกรอกจำนวนเงินแล้ว ระบบจะโพกัสช่อง “บันทึกช่วยจำ”
+                  อัตโนมัติ
+                </Text>
+              </VStack>
+              <Switch
+                value={isAutoFocusNote}
+                onToggle={toggleAutoFocusNote}
+                trackColor={{
+                  false: colors.blue[300],
+                  true: colors.blue[500],
+                }}
+                thumbColor={colors.blue[100]}
+                ios_backgroundColor={colors.blue[300]}
+              />
+            </HStack>
+          </VStack>
 
-        <VStack space="md">
-          <SignOutButton />
-          <Center>
-            <AppVersion />
-          </Center>
+          <VStack space="md">
+            <SignOutButton />
+            <Center>
+              <AppVersion />
+            </Center>
+          </VStack>
         </VStack>
-      </VStack>
+      </ScrollView>
     </SafeAreaView>
   );
 }
