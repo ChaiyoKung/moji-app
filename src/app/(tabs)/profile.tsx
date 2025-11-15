79634dc6c71eac3e82e8784de13c25a61b09109b
ChaiyoKung <kananek45@gmail.com>
2025-11-16 00:26:24 +0700

    feat(profile): add hide balance toggle switch to profile screen
    Added a switch to the profile screen allowing users to hide their balance. Utilizes `useHideBalanceStore` for state management and improves privacy options.

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -4,13 +4,39 @@ import { SignOutButton } from "../../features/sign-out-button";
 import { ProfileDetails } from "../../features/profile-details";
 import { AppVersion } from "../../features/app-version";
 import { Center } from "../../components/ui/center";
+import { HStack } from "../../components/ui/hstack";
+import { Text } from "../../components/ui/text";
+import { Switch } from "../../components/ui/switch";
+import { useHideBalanceStore } from "../../stores/use-hide-balance-store";
+import colors from "tailwindcss/colors";
 
 export default function Profile() {
+  const isBalanceHidden = useHideBalanceStore((state) => state.isBalanceHidden);
+  const toggleHideBalance = useHideBalanceStore(
+    (state) => state.toggleHideBalance
+  );
+
   return (
     <SafeAreaView edges={["top"]} className="flex-1 bg-background-100">
       <VStack space="4xl" className="flex-1 p-4">
-        <VStack space="md" className="flex-1">
+        <VStack space="md">
           <ProfileDetails />
+        </VStack>
+
+        <VStack space="md" className="flex-1">
+          <HStack className="items-center rounded-2xl border border-gray-300 px-4 py-2">
+            <Text className="flex-1">ซ่อนเงินคงเหลือ</Text>
+            <Switch
+              value={isBalanceHidden}
+              onToggle={toggleHideBalance}
+              trackColor={{
+                false: colors.blue[300],
+                true: colors.blue[500],
+              }}
+              thumbColor={colors.blue[100]}
+              ios_backgroundColor={colors.blue[300]}
+            />
+          </HStack>
         </VStack>
 
         <VStack space="md">
