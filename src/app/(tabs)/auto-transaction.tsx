4b7c45625cc94b7128d00e65d183966eec57c094
ChaiyoKung <kananek45@gmail.com>
2026-06-27 00:39:57 +0700

    feat: register auto-transaction tab in bottom navigator

---
+++ src/app/(tabs)/auto-transaction.tsx
@@ -0,0 +1,5 @@
+import { AutoTransactionScreen } from "../../features/auto-transaction";
+
+export default function AutoTransactionPage() {
+  return <AutoTransactionScreen />;
+}
---
+++ src/features/auto-transaction/index.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "../../components/ui/text";
+
+export function AutoTransactionScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-background-0">
+      <Text>Auto Transaction</Text>
+    </View>
+  );
+}
